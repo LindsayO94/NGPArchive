eda02bc473b5021ab22b1101277abdc63e985598
Lindsay <lindsayrose03@yahoo.com>
2016-08-24 07:18:59 -0400

    New titles

--- app/app.js
+++ app/app.js
@@ -44,77 +44,77 @@ app.controller("archiveController", function($scope) {
 		date: "July 5th, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1233805329965327/"
 	},{
-		title: "New Game Plus 59",
+		title: "New Game Plus 59: Burritos are good edition",
 		date: "June 28th, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1229426060403254/"
 	},
 	{
-		title: "New Game Plus 58",
+		title: "New Game Plus 58: Review us on Yelp!",
 		date: "June 21st, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1224928324186361/"
 	},
 	{
-		title: "New Game Plus 57",
+		title: "New Game Plus 57: F'ing Flag Day Edition",
 		date: "June 14th, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1220233234655870/"
 	},
 	{
-		title: "New Game Plus 56",
+		title: "New Game Plus 56: Doublemint Gum!",
 		date: "June 7th, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1215712608441266/"
 	},
 	{
-		title: "New Game Plus 55",
+		title: "New Game Plus 55: Summer time and living is easy",
 		date: "May 31st, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1211045502241310/"
 	},
 	{
-		title: "New Game Plus 54",
+		title: "New Game Plus 54: Icies rejoice; One less Peach player edition",
 		date: "May 24th, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1206662312679629/"
 	},
 	{
-		title: "New Game Plus 53",
+		title: "New Game Plus 53: Where have all the flowers gone?",
 		date: "May 17th, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1202045639807963/"
 	},
 	{
-		title: "New Game Plus 52",
+		title: "New Game Plus 52: Where's the beef?",
 		date: "May 10th, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1197338473612013/"
 	},
 	{
-		title: "New Game Plus 51",
+		title: "New Game Plus 51: It's MORPHIN TIME",
 		date: "May 3rd, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1192594967419697/"
 	},
 	{
-		title: "New Game Plus 50",
+		title: "New Game Plus 50: Let's go crazy",
 		date: "April 26th, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1188053421207185/"
 	},
 	{
-		title: "New Game Plus 49",
+		title: "New Game Plus 49: Do your taxes",
 		date: "April 12nd, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1179118748767319/"
 	},
 	{
-		title: "New Game Plus 48",
+		title: "New Game Plus 48: How do I Slox edition",
 		date: "April 5th, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1174331255912735/"
 	},
 	{
-		title: "New Game Plus 47",
+		title: "New Game Plus 47: Did France kill Mafia?",
 		date: "March 29th, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1167647706581090/"
 	},
 	{
-		title: "New Game Plus 46",
+		title: "New Game Plus 46: We love you New England Melee edition",
 		date: "March 22nd, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1159463550732839/"
 	},
 	{
-		title: "New Game Plus 45",
+		title: "New Game Plus 45 - Special Announcement Edition",
 		date: "March 15th, 2016",
 		link: "https://www.facebook.com/groups/NewEnglandMelee/permalink/1153359794676548/"
 	},
